d02b657dc5e63178d28a2ef6a8a359e6d4642d24
sumonmia360 <sumonmia4526@gmail.com>
2025-10-14 13:32:16 +0600

    ...

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const profile = () => {
   return (
     <View className="flex-1 items-center  p-3 bg-white">
-      <Text className="text-dark-200 text-4xl">profile123</Text>
+      <Text className="text-dark-200 text-4xl">profile</Text>
     </View>
   );
 };
